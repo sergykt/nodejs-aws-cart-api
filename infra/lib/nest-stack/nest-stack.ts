3eb5045d48439168dccfdff02a8cadb4aee85509
Sergei Sivtsev <sergykt@gmail.com>
2026-05-26 00:44:16 +0600

    feat: integrate PostgreSQL database with TypeORM and Secrets Manager
    - Added AWS Secrets Manager for managing database credentials.
    - Created a VPC for the Lambda function.
    - Configured an RDS PostgreSQL instance with necessary settings.
    - Updated the NestJS Lambda function to connect to the RDS instance and read credentials from Secrets Manager.
    - Integrated TypeORM into the NestJS application for database interactions.
    - Updated app.module.ts to configure TypeORM with environment variables.
    - Added necessary dependencies for TypeORM and PostgreSQL.

--- infra/lib/nest-stack/nest-stack.ts
+++ infra/lib/nest-stack/nest-stack.ts
@@ -1,13 +1,67 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import * as path from 'path';
 
 export class NestStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // Create a Secrets Manager secret for database credentials
+    const dbCredentialsSecret = new secretsmanager.Secret(this, 'MyDBCreds', {
+      secretName: 'MyDBCredsName',
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({
+          username: 'myadminuser',
+        }),
+        generateStringKey: 'password',
+        excludePunctuation: true,
+        includeSpace: false,
+      },
+    });
+
+    // Create a VPC for the Lambda function
+    const vpc = new ec2.Vpc(this, 'MyVPC', {
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'PublicSubnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
+
+    // Create a RDS instance for the NestJS application
+    const dbInstance = new rds.DatabaseInstance(this, 'RDSInstance', {
+      engine: rds.DatabaseInstanceEngine.postgres({
+        version: rds.PostgresEngineVersion.VER_16,
+      }),
+      databaseName: 'mydb',
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.BURSTABLE3,
+        ec2.InstanceSize.MICRO,
+      ),
+      vpc,
+      credentials: rds.Credentials.fromSecret(dbCredentialsSecret),
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      multiAz: false,
+      allocatedStorage: 20,
+      maxAllocatedStorage: 100,
+      allowMajorVersionUpgrade: false,
+      autoMinorVersionUpgrade: true,
+      backupRetention: cdk.Duration.days(7),
+      deleteAutomatedBackups: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      deletionProtection: false,
+    });
 
     // Create the API Gateway REST API
     const apiGateway = new apigateway.RestApi(this, 'NestServiceApiGateway', {
@@ -15,29 +69,50 @@ export class NestStack extends cdk.Stack {
       description: 'API for the Nest Service',
     });
 
+    const projectRoot = path.join(__dirname, '../../../');
+
     // Create the Lambda function for NestJS
     const nestJsLambdaFunction = new lambdaNodejs.NodejsFunction(
       this,
       'NestJsLambdaFunction',
       {
         runtime: lambda.Runtime.NODEJS_20_X,
-        entry: path.join(__dirname, '../../src/handler.ts'),
-        projectRoot: path.join(__dirname, '../../'),
+        entry: path.join(projectRoot, 'src/handler.ts'),
+        projectRoot,
         handler: 'handler',
         bundling: {
           minify: true,
           sourceMap: false,
           externalModules: [
-            '@nestjs/websockets',
-            '@nestjs/websockets/socket-module',
             '@nestjs/microservices',
-            '@nestjs/microservices/microservices-module',
+            '@nestjs/websockets/socket-module',
+            'cache-manager',
             'class-transformer',
             'class-validator',
+            'expo-sqlite',
           ],
         },
+        vpc,
+        allowPublicSubnet: true,
+        securityGroups: [dbInstance.connections.securityGroups[0]],
+        environment: {
+          DB_HOST: dbInstance.dbInstanceEndpointAddress,
+          DB_PORT: dbInstance.dbInstanceEndpointPort,
+          DB_NAME: 'mydb',
+          DB_USERNAME: dbCredentialsSecret
+            .secretValueFromJson('username')
+            .unsafeUnwrap(),
+          DB_PASSWORD: dbCredentialsSecret
+            .secretValueFromJson('password')
+            .unsafeUnwrap(),
+        },
+        timeout: cdk.Duration.seconds(30),
       },
     );
+
+    // Allow the Lambda function to connect to the RDS instance and read the database credentials secret
+    dbInstance.connections.allowDefaultPortFrom(nestJsLambdaFunction);
+    dbCredentialsSecret.grantRead(nestJsLambdaFunction);
 
     // Integrate the Lambda function with API Gateway
     const nestJsLambdaIntegration = new apigateway.LambdaIntegration(
@@ -49,6 +124,18 @@ export class NestStack extends cdk.Stack {
     apiGateway.root
       .addResource('{proxy+}')
       .addMethod('ANY', nestJsLambdaIntegration);
+
+    new cdk.CfnOutput(this, 'DbCredentialsSecretArn', {
+      value: dbCredentialsSecret.secretArn,
+      description: 'The ARN of the database credentials secret',
+      exportName: 'DbCredentialsSecretArn',
+    });
+
+    new cdk.CfnOutput(this, 'RdsInstanceEndpoint', {
+      value: dbInstance.dbInstanceEndpointAddress,
+      description: 'The endpoint address of the RDS instance',
+      exportName: 'RdsInstanceEndpoint',
+    });
 
     new cdk.CfnOutput(this, 'NestApiGatewayUrl', {
       value: apiGateway.url,
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 
 import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
-import { ConfigModule } from '@nestjs/config';
-
 @Module({
-  imports: [AuthModule, CartModule, OrderModule, ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.getOrThrow<string>('DB_HOST'),
+        port: Number(configService.get<string>('DB_PORT') ?? '5432'),
+        username: configService.getOrThrow<string>('DB_USERNAME'),
+        password: configService.getOrThrow<string>('DB_PASSWORD'),
+        database: configService.getOrThrow<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+    AuthModule,
+    CartModule,
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
